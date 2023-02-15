bd778137076c10386540cbbe3f93226d1223a600
anupongaware <anupong.p@aware.co.th>
2023-02-15 14:09:39 +0700

    finished learning jsx

--- react_session/learn-react-01/src/index.js
+++ react_session/learn-react-01/src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+function greeting(name) {
+  return <p>Hello {name}</p>;
+}
+
+const nickname = "John";
+const element = (
+  <div>
+    <h1>Hello JSX and {nickname}</h1>
+    <button>This is a button</button>
+    <h2>{greeting("Ice Kub")}</h2>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
+
+/* 
+<React.StrictMode>
     <App />
   </React.StrictMode>
-);
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
